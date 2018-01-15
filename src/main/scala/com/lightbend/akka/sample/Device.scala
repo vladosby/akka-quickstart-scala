71abdbcea2a69882581e1a0437256c24e31f7acf
vladislav <vladislavn.kievski@gmail.com>
2018-01-15 13:45:14 +0300

    Add device group, device and device manager

--- src/main/scala/com/lightbend/akka/sample/Device.scala
+++ src/main/scala/com/lightbend/akka/sample/Device.scala
@@ -14,18 +14,29 @@ object Device {
 
 class Device(groupId: String, deviceId: String) extends Actor with ActorLogging {
   import Device._
+
   var lastTemperatureReading: Option[Double] = None
 
   override def preStart(): Unit = log.info("Device actor {}-{} started", groupId, deviceId)
+
   override def postStop(): Unit = log.info("Device actor {}-{} stopped", groupId, deviceId)
 
   override def receive: Receive = {
-    case RecordTemperature(id, value) ⇒
+    case DeviceManager.RequestTrackDevice(`groupId`, `deviceId`) =>
+      sender() ! DeviceManager.DeviceRegistered
+
+    case DeviceManager.RequestTrackDevice(groupId, deviceId) =>
+      log.warning(
+        "Ignoring TrackDevice request for {}-{}.This actor is responsible for {}-{}.",
+        groupId, deviceId, this.groupId, this.deviceId
+      )
+
+    case RecordTemperature(id, value) =>
       log.info("Recorded temperature reading {} with {}", value, id)
       lastTemperatureReading = Some(value)
       sender() ! TemperatureRecorded(id)
 
-    case ReadTemperature(id) ⇒
+    case ReadTemperature(id) =>
       sender() ! RespondTemperature(id, lastTemperatureReading)
   }
 }
